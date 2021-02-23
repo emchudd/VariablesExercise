23938f3ea56918b90c9892552b7b76fb44aa362e
emchudd <eclhuddleston@gmail.com>
2021-02-23 12:18:01 -0600

    completed exercise + bonus

---
+++ variablesExercise.js
@@ -0,0 +1,23 @@
+//const
+
+let num = 20;
+num = 15;
+num = 15 + 7;
+num--;
+num = "This is not a number"
+console.log(num);
+
+let blank = undefined;
+console.log(blank);
+
+blank = null;
+console.log(null);
+
+//BONUS
+
+var [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+console.log(letters.length);
+console.log(letters[9]);
